e52eebab0e700ed6a084b97ca8e053622eb2b52e
alpha <godspowerlawrence008@gmail.com>
2025-06-21 21:30:53 -0400

    first commit

---
+++ src/components/AdminSidebar.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './components.css'
+
+import { ChartBarIcon } from '@heroicons/react/24/outline';
+import { DocumentTextIcon, CalendarDaysIcon } from '@heroicons/react/20/solid';
+
+import { HomeIcon ,UserIcon  ,LockClosedIcon,CogIcon, StarIcon, ChevronDownIcon , BellIcon, ChatBubbleLeftRightIcon, ArrowRightOnRectangleIcon , ArrowUpOnSquareStackIcon , CalendarIcon} from '@heroicons/react/24/solid'
+import { FaUserCircle } from 'react-icons/fa';
+import { FaUserDoctor } from 'react-icons/fa6';
+
+
+const DocSideBar = () => {
+  return (
+    <>
+        <div className='poppins space-y-3 bg-white text-[18px] md:block hidden rounded w-1/3 h-[80%]'>
+                    
+                     
+                    
+                      <div className='flex px-8 w-full text-[#757575]  py-3 '>
+                      <HomeIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Dashboard</Link>
+                     
+                      </div>
+                    
+                      <div className='flex px-8 w-full text-[#757575] py-3'>
+                      <CalendarDaysIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Appointments</Link>
+                      </div>
+                    
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <FaUserDoctor className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Doctors</Link>
+                      </div>
+
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <UserIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>My Patients</Link>
+                      </div>
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <FaUserCircle className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Specialties</Link>
+                      </div>
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <StarIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Reviews</Link>
+                     
+                      </div>
+                    
+                     
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <ChartBarIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Transactions</Link>
+                     
+                      </div>
+                    
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <DocumentTextIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Invoice Reports</Link>
+                     
+                      </div>
+                    
+                      <div className='flex px-8 w-full text-[#757575] py-3 '>
+                      <CogIcon className="h-6 w-6 mr-5 " />
+                      <Link to="#" className='text-18px'>Settings</Link>
+                      </div>
+                    
+                     
+        
+                  </div>
+    </>
+  )
+}
+
+export default DocSideBar
